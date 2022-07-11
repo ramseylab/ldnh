92aed8381406fb8302216ee67a6545e0c6e4d215
Stephen Ramsey <stephen.ramsey@oregonstate.edu>
2022-07-11 16:16:42 -0700

    adding new function `process_voltammograms`

--- R/ldnh.R
+++ R/ldnh.R
@@ -4,3 +4,11 @@
 function() {}  ## this is required in order to force roxygen2::roxygenise to
                ## generate "import" directives in the NAMESPACE file
 
+#' Run the script `process-voltammograms.R` that is installed in the `ldnh` package.
+#' @export
+#' @examples
+#' process_voltammograms()
+
+process_voltammograms <- function() {
+    source(paste(.libPaths(), "ldnh/exec/process-voltammograms.R", sep="/"))
+}
